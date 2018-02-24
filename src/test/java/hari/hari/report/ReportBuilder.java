db163a77d37f24750da1739e645e18b4b8221f10
Hari <hariprasad.srinivas@prudential.co.uk>
2018-02-24 17:11:03 +0000

    initial commit - make sure eclipse project with these files...

---
+++ src/test/java/hari/hari/report/ReportBuilder.java
@@ -0,0 +1,51 @@
+package hari.hari.report;
+
+import java.lang.reflect.Method;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ReportBuilder {
+	
+	private static ExtentReports extent;
+	private static ThreadLocal parentTest = new ThreadLocal();
+    private static ThreadLocal test = new ThreadLocal();
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		extent = ExtentManager.createInstance("./test-output/extent-report.html");
+	}
+	
+	@BeforeClass
+    public synchronized void beforeClass() {
+        ExtentTest parent = extent.createTest(getClass().getName());
+        parentTest.set(parent);
+    }
+	
+	@BeforeMethod
+    public synchronized void beforeMethod(Method method) {
+        ExtentTest child = ((ExtentTest) parentTest.get()).createNode(method.getName());
+        test.set(child);
+    }
+	
+	@AfterMethod
+    public synchronized void afterMethod(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE)
+            ((ExtentTest) test.get()).fail(result.getThrowable());
+        else if (result.getStatus() == ITestResult.SKIP)
+            ((ExtentTest) test.get()).skip(result.getThrowable());
+        else
+            ((ExtentTest) test.get()).pass("Test passed");
+        extent.flush();
+    }
+	
+
+}
+
---
+++ src/test/java/hari/hari/report/testSuite.java
@@ -0,0 +1,55 @@
+package hari.hari.report;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class testSuite extends ReportBuilder {
+
+	
+	@Test
+	public  void Test1() {
+		
+		System.out.println("Pass");
+		
+	}
+		
+	@Test
+	public  void Test2() {
+		
+		System.out.println("Pass");
+	}
+	
+	@Test
+	public  void Test3() {
+		
+		System.out.println("Pass");
+	}
+	
+	@Test
+	public  void Test4() {
+		
+		System.out.println("Pass");
+	}
+	
+	@Test
+	public  void Test5() {
+		
+		System.out.println("Pass");
+	}
+	
+	@Test
+	public  void Test6() {
+		
+		System.out.println("Pass");
+	}
+	
+
+}
